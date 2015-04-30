ebf079af3d7f225c44ca4d8d1c5c530363bc2190
Anji Jonnala <anjir@codeaurora.org>
2015-04-30 17:14:40 +0530

    drivers: lpm-workarounds: Misc bug fixes
    1)Processing all cpu hotplug events leading to lpm_wa workqueue
    getting scheduled multiple times and obseving the below warning message.
    [22.136470] process_lpm_workarounds: perf L2 is not in low power mode
    [22.138296] process_lpm_workarounds: perf L2 is not in low power mode
    Fix this by ignorning all hotplug events except CPU_POST_DEAD and
    CPU_ONLINE.
    2) L1_l2_gcc register is secure on few chipsets, issue scm_io_write
    to enable this.
    3) Remove the lpmcx turbo vote WA.
    
    Change-Id: I02a1a5af086fbb7f5782f51bff9f8b731e352125
    Signed-off-by: Anji Jonnala <anjir@codeaurora.org>

--- drivers/cpuidle/lpm-workarounds.c
+++ drivers/cpuidle/lpm-workarounds.c
@@ -24,15 +24,15 @@
 #include <linux/msm_thermal.h>
 #include <linux/delay.h>
 #include <linux/cpu.h>
+#include <soc/qcom/scm.h>
 
 #define L2_HS_STS_SET	0x200
 
-static struct regulator *lpm_cx_reg;
 static struct work_struct lpm_wa_work;
 static struct workqueue_struct *lpm_wa_wq;
-static bool lpm_wa_cx_turbo_unvote;
 static bool skip_l2_spm;
 static bool enable_dynamic_clock_gating;
+static bool is_l1_l2_gcc_secure;
 static bool store_clock_gating;
 int non_boot_cpu_index;
 void __iomem *l2_pwr_sts;
@@ -41,18 +41,25 @@ struct device_clnt_data      *hotplug_handle;
 union device_request curr_req;
 cpumask_t l1_l2_offline_mask;
 cpumask_t offline_mask;
+struct resource *l1_l2_gcc_res;
+uint32_t l2_status = -1;
 
 static int lpm_wa_callback(struct notifier_block *cpu_nb,
 	unsigned long action, void *hcpu)
 {
 	unsigned long cpu = (unsigned long) hcpu;
 
+	if ((action != CPU_POST_DEAD) && (action != CPU_ONLINE))
+		return NOTIFY_OK;
 	if (cpu >= non_boot_cpu_index)
 		return NOTIFY_OK;
 
 	switch (action & ~CPU_TASKS_FROZEN) {
-	case CPU_DEAD:
+	case CPU_POST_DEAD:
 		cpumask_set_cpu(cpu, &offline_mask);
+		break;
+	case CPU_ONLINE:
+		cpumask_clear_cpu(cpu, &offline_mask);
 		break;
 	default:
 		break;
@@ -73,22 +80,6 @@ static void process_lpm_workarounds(struct work_struct *w)
 {
 	int ret = 0, status = 0;
 
-	/* While exiting from RPM assisted power collapse on some targets
-	 * like MSM8939 the CX is bumped to turbo mode by RPM. To reduce
-	 * the power impact, APSS low power driver need to remove the CX
-	 * turbo vote.
-	 */
-
-	if (lpm_wa_cx_turbo_unvote && lpm_cx_reg) {
-		regulator_set_voltage(lpm_cx_reg,
-			RPM_REGULATOR_CORNER_SUPER_TURBO,
-			RPM_REGULATOR_CORNER_SUPER_TURBO);
-
-		regulator_set_voltage(lpm_cx_reg,
-			RPM_REGULATOR_CORNER_NONE,
-			RPM_REGULATOR_CORNER_SUPER_TURBO);
-	}
-
 	/* MSM8952 have L1/L2 dynamic clock gating disabled in HW for
 	 * performance cluster cores. Enable it via SW to reduce power
 	 * impact.
@@ -105,16 +96,23 @@ static void process_lpm_workarounds(struct work_struct *w)
 		if (status) {
 			pr_err("%s: perf L2 is not in low power mode\n",
 								__func__);
+			queue_work(lpm_wa_wq, &lpm_wa_work);
 			return;
 		}
 
-		pr_info("Set L1_L2_GCC from cpu%d when perf L2 status=0x%x\n",
-			smp_processor_id(), __raw_readl(l2_pwr_sts));
-		__raw_writel(0x0, l1_l2_gcc);
+		l2_status = __raw_readl(l2_pwr_sts);
+		pr_err("Set L1_L2_GCC from cpu%d when perf L2 status=0x%x\n",
+			smp_processor_id(), l2_status);
 
-		if (__raw_readl(l1_l2_gcc) != 0x0)
-			pr_err("Failed to set L1_L2_GCC\n");
-
+		if (is_l1_l2_gcc_secure) {
+			scm_io_write((u32)(l1_l2_gcc_res->start), 0x0);
+			if (scm_io_read((u32)(l1_l2_gcc_res->start)) != 0)
+				pr_err("Failed to set L1_L2_GCC\n");
+		} else {
+			__raw_writel(0x0, l1_l2_gcc);
+			if (__raw_readl(l1_l2_gcc) != 0x0)
+				pr_err("Failed to set L1_L2_GCC\n");
+		}
 		HOTPLUG_NO_MITIGATION(&curr_req.offline_mask);
 		ret = devmgr_client_request_mitigation(
 				hotplug_handle,
@@ -129,17 +127,6 @@ static void process_lpm_workarounds(struct work_struct *w)
 	}
 }
 
-/*
- * lpm_wa_skip_l2_spm: Dont program the l2 SPM as TZ is programming the
- * L2 SPM as a workaround for SDI fix.
- */
-bool lpm_wa_get_skip_l2_spm(void)
-{
-	if (lpm_wa_cx_turbo_unvote)
-		queue_work(lpm_wa_wq, &lpm_wa_work);
-}
-EXPORT_SYMBOL(lpm_wa_get_skip_l2_spm);
-
 bool lpm_wa_get_skip_l2_spm(void)
 {
 	return skip_l2_spm;
@@ -147,35 +134,6 @@ bool lpm_wa_get_skip_l2_spm(void)
 EXPORT_SYMBOL(lpm_wa_get_skip_l2_spm);
 
 
-static int lpm_wa_cx_unvote_init(struct platform_device *pdev)
-{
-	int ret = 0, val = 0;
-
-	ret = kstrtoint(buf, 10, &val);
-	if (ret || !enable_dynamic_clock_gating) {
-		pr_err("Invalid input%s %s. err:%d\n", __func__, buf, ret);
-		return count;
-	}
-	cpumask_copy(&curr_req.offline_mask, &l1_l2_offline_mask);
-	ret = devmgr_client_request_mitigation(
-			hotplug_handle,
-			HOTPLUG_MITIGATION_REQ,
-			&curr_req);
-	if (ret) {
-		pr_err("hotplug request failed. err:%d\n", ret);
-		return count;
-	}
-
-	return ret;
-}
-
-static int lpm_wa_cx_unvote_exit(void)
-{
-	if (lpm_wa_wq)
-		destroy_workqueue(lpm_wa_wq);
-
-	return 0;
-}
 static ssize_t store_clock_gating_enabled(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
@@ -212,6 +170,7 @@ static int lpm_wa_probe(struct platform_device *pdev)
 	struct resource *res = NULL;
 	unsigned long cpu_mask = 0;
 	char *key;
+
 
 	skip_l2_spm = of_property_read_bool(pdev->dev.of_node,
 					"qcom,lpm-wa-skip-l2-spm");
@@ -252,68 +211,6 @@ static int lpm_wa_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 	l1_l2_gcc_res = res;
-
-	l1_l2_gcc = devm_ioremap(&pdev->dev, res->start,
-						resource_size(res));
-	if (!l1_l2_gcc) {
-		pr_err("%s: cannot ioremap L1/L2 GCC override %s\n",
-						__func__, KBUILD_MODNAME);
-		return -ENOMEM;
-	}
-
-	key = "qcom,non-boot-cpu-index";
-	ret = of_property_read_u32(pdev->dev.of_node, key,
-						&non_boot_cpu_index);
-	if (ret) {
-		pr_err("%s: Missing qcom,non_boot_cpu_index property\n"
-							, __func__);
-	}
-
-	key = "qcom,cpu-offline-mask";
-	ret = of_property_read_u32(pdev->dev.of_node, key,
-						(u32 *) &cpu_mask);
-	if (!ret) {
-		for_each_set_bit(c, &cpu_mask, num_possible_cpus()) {
-			cpumask_set_cpu(c, &l1_l2_offline_mask);
-		}
-	}
-
-	skip_l2_spm = of_property_read_bool(pdev->dev.of_node,
-					"qcom,lpm-wa-skip-l2-spm");
-
-	/*
-	 * Enabling L1/L2 tag ram clock gating requires core and L2 to be
-	 * in quiescent state. lpm-wa-dynamic-clock-gating flag specifies
-	 * WA implementation in SW for perf core0 and L2.
-	 */
-	enable_dynamic_clock_gating = of_property_read_bool(pdev->dev.of_node,
-					"qcom,lpm-wa-dynamic-clock-gating");
-	if (!enable_dynamic_clock_gating)
-		return ret;
-
-	res = platform_get_resource_byname(pdev, IORESOURCE_MEM,
-							"l2-pwr-sts");
-	if (!res) {
-		pr_err("%s(): Missing perf l2-pwr-sts resource\n",
-							 __func__);
-		return -EINVAL;
-	}
-
-	l2_pwr_sts = devm_ioremap(&pdev->dev, res->start,
-						resource_size(res));
-	if (!l2_pwr_sts) {
-		pr_err("%s: cannot ioremap l2_pwr_status %s\n",	__func__,
-							KBUILD_MODNAME);
-		return -ENOMEM;
-	}
-
-	res = platform_get_resource_byname(pdev, IORESOURCE_MEM,
-							"l1-l2-gcc");
-	if (!res) {
-		pr_err("%s(): Missing L1/L2 GCC override resource\n",
-								__func__);
-		return -EINVAL;
-	}
 
 	l1_l2_gcc = devm_ioremap(&pdev->dev, res->start,
 						resource_size(res));
