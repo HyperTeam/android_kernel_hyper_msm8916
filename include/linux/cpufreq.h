afbc09a9fdd88fd9624854bce203ac0321b96642
jamison904 <james.jamison85@gmail.com>
2016-04-15 10:50:58 +0530

    cpufreq: Add BIOSHOCK governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -464,6 +464,9 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bioshock)
 #endif
 
 /*********************************************************************
