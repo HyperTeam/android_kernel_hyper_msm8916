c07626b04849dc1430220a4e176c27dfe83177c4
Nathan Chancellor <natechancellor@gmail.com>
2017-02-22 18:46:16 -0700

    Initial dmesg pruning
    These are all of the annoying messages on just the stock kernel...
    
    More to follow in future patches!
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/power/battery_current_limit.c
+++ drivers/power/battery_current_limit.c
@@ -254,7 +254,7 @@ static int __ref bcl_cpu_ctrl_callback(struct notifier_block *nfb,
 	if (action == CPU_UP_PREPARE || action == CPU_UP_PREPARE_FROZEN) {
 		if ((bcl_hotplug_mask & BIT(cpu))
 			&& (bcl_hotplug_request & BIT(cpu))) {
-			pr_info("preventing CPU%d from coming online\n", cpu);
+			pr_debug("preventing CPU%d from coming online\n", cpu);
 			return NOTIFY_BAD;
 		} else {
 			pr_debug("voting for CPU%d to be online\n", cpu);
