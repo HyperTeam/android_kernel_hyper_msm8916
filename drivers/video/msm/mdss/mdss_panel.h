4837a331965cd7815e2516d0d3d501f24b5d62be
dev_harsh1998 <harshitjain6751@gmail.com>
2017-04-13 11:42:33 +0200

    drivers: video: Port lenovo modifications
    * Removed LiveDisplay Driver (causing Loops)
    * Add back lenovo's gamma control interface
    * Commonize the changes for lenovo wingtech Series
    
    Bugs addressed:
    * Flickering (Official Stock Roms S010 , S057 , S028)
    * Lock screen blank issue
    
    Status : Testing
    
    Data taken from :
    * LineageOS/android_kernel_lenovo_msm8916@7524a24a51beb8ba59a46be1cb85bb5b6a0b60d7
    
    Adapted for Lenovo A6000 by @dev-harsh1998
    
    Lenovo this source code is much better now when compared to your useless buggy source drops

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -366,8 +366,6 @@ struct mdss_mdp_pp_tear_check {
 	u32 refx100;
 };
 
-struct mdss_livedisplay_ctx;
-
 struct mdss_panel_info {
 	u32 xres;
 	u32 yres;
@@ -443,8 +441,6 @@ struct mdss_panel_info {
 
 	/* debugfs structure for the panel */
 	struct mdss_panel_debugfs_info *debugfs_info;
-
-	struct mdss_livedisplay_ctx *livedisplay;
 };
 
 struct mdss_panel_data {
