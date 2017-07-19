450ac58ee2ef2c98fb0cafed8cbd4d7fb0335d1c
beroid <beroid3@gmail.com>
2017-07-19 10:10:14 +0500

    slim-msm-ngd: Remove duplicate line

--- drivers/slimbus/slim-msm-ngd.c
+++ drivers/slimbus/slim-msm-ngd.c
@@ -1435,7 +1435,6 @@ static int ngd_slim_probe(struct platform_device *pdev)
 	init_completion(&dev->ctrl_up);
 	mutex_init(&dev->tx_lock);
 	mutex_init(&dev->tx_buf_lock);
-	mutex_init(&dev->ssr_lock);
 	spin_lock_init(&dev->rx_lock);
 	dev->ee = 1;
 	dev->irq = irq->start;
